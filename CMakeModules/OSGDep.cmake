303df7ed944e5ecfbf888232b7eb46c29891c996
leadcoder <leadcoder@gmail.com>
2015-01-22 23:32:00 +0100

    Changed from custom osg dependency resolver to native cmake command FIND_PACKAGE(OpenSceneGraph...

--- CMakeModules/OSGDep.cmake
+++ CMakeModules/OSGDep.cmake
@@ -1,42 +1,7 @@
 
-if(EXISTS "${GASS_DEPENDENCIES_DIR}/OpenSceneGraph-v3.2.1")
-	set(OSG_DIR  "${GASS_DEPENDENCIES_DIR}/OpenSceneGraph-v3.2.1" CACHE PATH "OSG folder")
-else()
-	set(OSG_DIR $ENV{OSG_HOME} CACHE PATH "OSG folder")
-endif()
-
 set(OSG_BIN_DIR ${OSG_DIR}/bin)
-set(OSG_INC_DIR ${OSG_DIR}/include)
-set(OSG_LIB_DIR ${OSG_DIR}/lib)
-
-set(OSG_LINK_LIBRARIES debug OpenThreadsd
-			debug osgd
-			debug osgDBd
-			debug osgUtild
-			debug osgGAd
-			debug osgViewerd
-			debug osgTextd
-			debug osgShadowd
-			debug osgSimd
-			debug osgTerraind
-			optimized OpenThreads
-			optimized osg
-			optimized osgDB
-			optimized osgUtil
-			optimized osgGA
-			optimized osgViewer
-			optimized osgText
-			optimized osgShadow
-			optimized osgSim
-			optimized osgTerrain
-			opengl32
-			glu32)
-
-			
-
 set(OSG_PLUGINS_DIR_NAME osgPlugins-3.2.1)
 set(OSG_PLUGINS_DIR	${OSG_BIN_DIR}/${OSG_PLUGINS_DIR_NAME})
-
 FILE(GLOB OSG_PLUGINS "${OSG_PLUGINS_DIR}/*.dll")
 
 set(OSG_BIN_FILES_DEBUG ${OSG_BIN_DIR}/ot20-OpenThreadsd.dll
@@ -76,6 +41,3 @@ ${OSG_BIN_DIR}/osg100-osgManipulator.dll
 ${OSG_BIN_DIR}/osg100-osgParticle.dll
 ${OSG_BIN_DIR}/osg100-osgVolume.dll
 )
-
-#set(OSG_LIB_DIR ${OSG_BIN_DIR}/lib)
-#set(OSG_INC_DIR ${OSG_BIN_DIR}/include)
