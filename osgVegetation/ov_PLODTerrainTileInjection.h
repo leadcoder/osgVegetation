4ec2e23590c8094eb21cfbbaf4534f3f9ec207b9
leadcoder <leadcoder@gmailcom>
2019-01-29 20:53:36 +0100

    PLOD refactoring

--- osgVegetation/ov_PLODTerrainTileInjection.h
+++ osgVegetation/ov_PLODTerrainTileInjection.h
@@ -11,45 +11,14 @@
 
 namespace osgVegetation
 {
-	class LODLayer
-	{
-	public:
-		LODLayer(int lod_level, std::vector<BillboardLayer> layers) : LODLevel(lod_level),
-			Generator(layers) {}
-		int LODLevel;
-		BillboardNodeGenerator Generator;
-	};
-
 	class PLODTerrainTileInjection : public osgDB::ReadFileCallback
 	{
 	public:
-
-		PLODTerrainTileInjection(const std::vector<LODLayer> &lod_layers) : m_LODLayers(lod_layers)
-		{
-			
-		}
-	
-		PLODTerrainTileInjection(const std::vector<BillboardLayer> &layers) 
-		{
-			typedef std::map<int, std::vector<BillboardLayer> > LODBuckets;
-			LODBuckets lod_buckets;
-			for (size_t i = 0; i < layers.size(); i++)
-			{
-				lod_buckets[layers[i].LODLevel].push_back(layers[i]);
-			}
-			
-			for (LODBuckets::const_iterator iter = lod_buckets.begin(); iter != lod_buckets.end(); iter++)
-			{
-				m_LODLayers.push_back(LODLayer(iter->first, iter->second));
-			}
-		}
-
-		class TerrainTileVisitor : public osg::NodeVisitor
+		class PLODGeometryCreator : public osg::NodeVisitor
 		{
 		public:
-			std::vector<osgTerrain::TerrainTile*> Tiles;
-			osg::Group* MainGroup;
-			TerrainTileVisitor() :
+			osg::Group* CurrentParent;
+			PLODGeometryCreator(osg::Group* root) : CurrentParent(root),
 				osg::NodeVisitor(osg::NodeVisitor::TRAVERSE_ALL_CHILDREN) {
 			}
 
@@ -58,14 +27,49 @@ namespace osgVegetation
 				osgTerrain::TerrainTile* tile = dynamic_cast<osgTerrain::TerrainTile*>(&node);
 				if (tile)
 				{
-					Tiles.push_back(tile);
+					if (osg::Node* terrain_tile = TerrainHelper::CreateTerrainNodeFromTerrainTile(tile))
+						CurrentParent->addChild(terrain_tile);
 				}
 				else
 				{
 					traverse(node);
 				}
 			}
+
+			void apply(osg::Geode& geode)
+			{
+				if (osg::Geode* geode_copy = dynamic_cast<osg::Geode*>(geode.clone(osg::CopyOp::DEEP_COPY_ALL)))
+				{
+					osg::MatrixTransform* new_matrix = new osg::MatrixTransform();
+					osg::NodePath nodePath = getNodePath();
+					new_matrix->setMatrix(osg::computeLocalToWorld(nodePath));
+					new_matrix->addChild(geode_copy);
+					CurrentParent->addChild(new_matrix);
+				}
+			}
 		};
+
+
+		PLODTerrainTileInjection(const std::map<int, BillboardNodeGenerator> &lod_layers) : m_LODLayers(lod_layers)
+		{
+
+		}
+
+		PLODTerrainTileInjection(const std::vector<BillboardLayer> &layers)
+		{
+			typedef std::map<int, std::vector<BillboardLayer> > LODBuckets;
+			LODBuckets lod_buckets;
+			for (size_t i = 0; i < layers.size(); i++)
+			{
+				lod_buckets[layers[i].LODLevel].push_back(layers[i]);
+			}
+
+			for (LODBuckets::const_iterator iter = lod_buckets.begin(); iter != lod_buckets.end(); iter++)
+			{
+				m_LODLayers.insert(std::pair<int, BillboardNodeGenerator>(iter->first, BillboardNodeGenerator(iter->second)));
+			}
+		}
+	
 
 		static int extractLODLevelFromFileName(const std::string& filename)
 		{
@@ -90,49 +94,51 @@ namespace osgVegetation
 			return lod_level;
 		}
 
-#define OV_USE_TILE_ID_LOD_LEVEL
+		osg::Group*  CreateTerrainGeometry(osg::Node* terrain)
+		{
+			osg::Group* terrain_geometry = new osg::Group();
+			PLODGeometryCreator visitor(terrain_geometry);
+			terrain->accept(visitor);
+
+			//Prepare for tesselation shaders, change PrimitiveSet draw mode to GL_PATCHES
+			ConvertToPatchesVisitor cv;
+			terrain_geometry->accept(cv);
+			return terrain_geometry;
+		}
+
+		//#define OV_USE_TILE_ID_LOD_LEVEL
 		virtual osgDB::ReaderWriter::ReadResult readNode(const std::string& filename, const osgDB::Options* options)
 		{
 			osgDB::ReaderWriter::ReadResult rr = ReadFileCallback::readNode(filename, options);
 
 			if (!rr.getNode())
 				return rr;
-			TerrainTileVisitor ttv;
-			rr.getNode()->accept(ttv);
-			
-			for (size_t i = 0; i < ttv.Tiles.size(); i++)
-			{
-				//osgVegetation::PrepareTerrainForDetailMapping(ttv.Tiles[i],m_Terrain);
-			}
-			
+
+			if (!rr.validNode())
+				return rr;
+
 #ifdef OV_USE_TILE_ID_LOD_LEVEL
 			const int lod_level = ttv.Tiles.size() > 0 ? ttv.Tiles[0]->getTileID().level - 1 : 0;
 #else
 			const int lod_level = extractLODLevelFromFileName(filename);
 #endif
-			for (size_t i = 0; i < m_LODLayers.size(); i++)
+			std::map<int, BillboardNodeGenerator>::const_iterator iter = m_LODLayers.find(lod_level);
+			if (iter != m_LODLayers.end())
 			{
-				if (lod_level == m_LODLayers[i].LODLevel && rr.validNode())
+				osg::Group* root_node = dynamic_cast<osg::Group*>(rr.getNode());
+				osg::PagedLOD* plod = dynamic_cast<osg::PagedLOD*>(root_node);  
+				if (root_node && plod == NULL) //check that root node is a group-node, also check if PagedLOD (leaf nodes?), then not possible to attach 
 				{
-					osg::Group* group = dynamic_cast<osg::Group*>(rr.getNode());
-					osg::PagedLOD* plod = dynamic_cast<osg::PagedLOD*>(rr.getNode());
-					if (group && plod == 0)
-					{
-						osg::Group* terrain_node_group = new osg::Group();
-						for (size_t j = 0; j < ttv.Tiles.size(); j++)
-						{
-							if(osg::Node* terrain_tile = TerrainHelper::CreateTerrainNodeFromTerrainTile(ttv.Tiles[j]))
-								terrain_node_group->addChild(terrain_tile);
-						}
-						osg::ref_ptr<osg::Node> veg_node = m_LODLayers[i].Generator.CreateNode(terrain_node_group);
-						group->addChild(veg_node);
-					}
+					//Create/clone terrain geometry
+					osg::Group* terrain_geometry = CreateTerrainGeometry(root_node);
+					osg::ref_ptr<osg::Node> veg_node = iter->second.CreateNode(terrain_geometry);
+					root_node->addChild(veg_node);
 				}
 			}
 			return rr;
 		}
 	protected:
 		virtual ~PLODTerrainTileInjection() {}
-		std::vector<LODLayer> m_LODLayers;
+		std::map<int, BillboardNodeGenerator> m_LODLayers;
 	};
 }
